15fb55c5b3bf6f0db8b1d4f66d9296bd42a5cfd2
moussabarry551728 <moussabarry551728@gmail.com>
2026-08-22 11:52:14 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "ICI_API_KEY",
+  apiKey: "AIzaSyAi1WnffifU9KHSCsoJ_sundZsY3yZ7yy4",
   authDomain: "fasopass.firebaseapp.com",
   projectId: "fasopass",
   storageBucket: "fasopass.firebasestorage.app",
-  messagingSenderId: "ICI_SENDER_ID",
-  appId: "ICI_APP_ID"
+  messagingSenderId: "775126269357",
+  appId: "1:775126269357:web:bb22c2065ce3ec1971b524",
+  measurementId: "G-1JS59XQSQ2"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export { app, db };
